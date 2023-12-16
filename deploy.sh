0a04261caa33eeaf928463fe2e392d69b5498b1e
abu <yageabu@163.com>
2023-12-16 10:32:51 +0800

    'deploy-code'

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yageabu/xiexinghuang.git main:gh-pages
+git push -f git@github.com:yageabu/xiexinghuang.git master:gh-pages
 
 cd -
 
