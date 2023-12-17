5af3c80ce156cf29c18e493cef91c2f9b0d315ee
abu <yageabu@163.com>
2023-12-17 16:07:05 +0800

    'deploy-12-17'

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,7 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+git init && git add -A && git commit -m 'deploy-12-17'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
